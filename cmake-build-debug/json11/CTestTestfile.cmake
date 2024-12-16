4dbd307012fb6917a1709813839ffb19bb8fc285
alegarcro <alegarcro@myuax.com>
2024-12-16 10:05:06 +0100

    Commit final 2

---
+++ cmake-build-debug/json11/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Alex Garcia/Desktop/NOCIONES CLAVE(2)/CasoIntegrador/json11
+# Build directory: C:/Users/Alex Garcia/Desktop/NOCIONES CLAVE(2)/CasoIntegrador/cmake-build-debug/json11
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
